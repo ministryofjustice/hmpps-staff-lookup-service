d06d66478b3bfed6f9feb7afaf1cd6f6e594c8be
carlov20 <carlov20@users.noreply.github.com>
2023-01-26 14:55:15 +0000

    added config for connecting to postgres, auth, swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,21 +4,44 @@ plugins {
 }
 
 configurations {
+  implementation { exclude(module = "spring-boot-starter-web") }
+  implementation { exclude(module = "spring-boot-starter-tomcat") }
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
 dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+  runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
+
+  implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.14")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.14")
+  implementation("org.springdoc:springdoc-openapi-security:1.6.14")
+
+  implementation("io.opentelemetry:opentelemetry-api:1.22.0")
+
+  runtimeOnly("org.flywaydb:flyway-core")
+  runtimeOnly("org.postgresql:r2dbc-postgresql:0.9.3.RELEASE")
+  runtimeOnly("org.postgresql:postgresql:42.5.1")
+
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+
+  testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
 }
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(18))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(19))
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "18"
+      jvmTarget = "19"
     }
   }
 }
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstafflookupservice/config/OpenApiConfiguration.kt
@@ -0,0 +1,31 @@
+package uk.gov.justice.digital.hmpps.hmppsstafflookupservice.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Contact
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.boot.info.BuildProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfiguration(buildProperties: BuildProperties) {
+  private val version: String = buildProperties.version
+
+  @Bean
+  fun customOpenAPI(): OpenAPI = OpenAPI()
+    .servers(
+      listOf(
+        Server().url("https://hmpps-staff-lookup-service-dev.hmpps.service.justice.gov.uk").description("Development"),
+        Server().url("https://hmpps-staff-lookup-service-preprod.hmpps.service.justice.gov.uk").description("PreProd"),
+        Server().url("https://hmpps-staff-lookup-service.hmpps.service.justice.gov.uk").description("Prod"),
+        Server().url("http://localhost:8080").description("Local"),
+      )
+    )
+    .info(
+      Info().title("HMPPS Staff Lookup API")
+        .version(version)
+        .description("HMPPS Staff")
+        .contact(Contact().name("HMPPS Digital Studio").email("feedback@digital.justice.gov.uk"))
+    )
+}
