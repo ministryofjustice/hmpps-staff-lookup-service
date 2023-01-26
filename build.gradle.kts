107689a7f293dc60247781e061effb4263f341d2
carlov20 <carlov20@users.noreply.github.com>
2023-01-26 14:59:36 +0000

    compile fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,13 +35,13 @@ dependencies {
 }
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(19))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(18))
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "19"
+      jvmTarget = "18"
     }
   }
 }
