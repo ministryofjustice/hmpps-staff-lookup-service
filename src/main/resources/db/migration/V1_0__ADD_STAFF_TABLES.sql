6c6055d7c1ff1758bc1af1c0443c7f771a664ad7
carlov20 <carlov20@users.noreply.github.com>
2023-01-27 16:45:18 +0000

    split flyway scripts, used staff table to create staff temp

--- src/main/resources/db/migration/V1_0__ADD_STAFF_TABLES.sql
+++ src/main/resources/db/migration/V1_0__ADD_STAFF_TABLES.sql
@@ -12,20 +12,8 @@ CREATE TABLE staff(
     email VARCHAR NOT NULL
 );
 
--- CONCURRENTLY builds the index without locking out writes
-CREATE INDEX CONCURRENTLY staff_email_idx ON staff USING gin (email gin_trgm_ops);
-
 drop table if exists staff_temp;
 
-CREATE TABLE staff_temp(
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    job_title VARCHAR,
-    email VARCHAR NOT NULL
-);
-
--- CONCURRENTLY builds the index without locking out writes
-CREATE INDEX CONCURRENTLY staff_temp_email_idx ON staff_temp USING gin (email gin_trgm_ops);
+CREATE TABLE staff_temp(LIKE staff INCLUDING ALL);
 
 
