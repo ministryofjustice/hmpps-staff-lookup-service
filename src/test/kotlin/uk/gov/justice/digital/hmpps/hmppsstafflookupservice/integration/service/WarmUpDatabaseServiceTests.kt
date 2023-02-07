f4d6a84636e2ef1751efd7e0afb460b368b9e938
carlov20 <carlov20@users.noreply.github.com>
2023-02-07 15:21:09 +0000

    WFP-1800: warm up after table swap (#28)
    * warm up after table swap
    
    * wrote unit test
    
    * made into int test

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsstafflookupservice/integration/service/WarmUpDatabaseServiceTests.kt
@@ -0,0 +1,28 @@
+package uk.gov.justice.digital.hmpps.hmppsstafflookupservice.integration.service
+
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.mock.mockito.MockBean
+import uk.gov.justice.digital.hmpps.hmppsstafflookupservice.db.repositories.StaffRepository
+import uk.gov.justice.digital.hmpps.hmppsstafflookupservice.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.hmppsstafflookupservice.service.WarmUpDatabaseService
+
+class WarmUpDatabaseServiceTests : IntegrationTestBase() {
+
+  @MockBean
+  override lateinit var staffRepository: StaffRepository
+
+  @Autowired
+  protected lateinit var warmUpDatabaseService: WarmUpDatabaseService
+
+  private val expectedCharacters = listOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+  @Test
+  fun `must query database with all lowercase alphabet characters`(): Unit = runBlocking {
+    warmUpDatabaseService.warmUpStaff()
+    for (character in expectedCharacters) {
+      Mockito.verify(staffRepository).findFirst20ByEmailLikeOrderByEmailAsc(character)
+    }
+  }
+}
