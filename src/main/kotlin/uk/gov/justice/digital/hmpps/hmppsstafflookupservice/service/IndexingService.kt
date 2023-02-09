9849e02723435050492a882d63fc5e81cbaac93d
carlov20 <carlov20@users.noreply.github.com>
2023-02-09 09:05:33 +0000

    WFP-1788 reverse alert test (#30)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstafflookupservice/service/IndexingService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstafflookupservice/service/IndexingService.kt
@@ -52,7 +52,7 @@ class IndexingService(
         telemetryClient.trackEvent(
           TelemetryEventType.INDEX_BUILD_COMPLETE.eventName,
           mapOf(
-            "success" to "false", // to test alert, revert once alert has gone off
+            "success" to successfulBuild.toString(),
             "timeTaken" to stopWatch.totalTimeSeconds.toString(),
           ),
           null
