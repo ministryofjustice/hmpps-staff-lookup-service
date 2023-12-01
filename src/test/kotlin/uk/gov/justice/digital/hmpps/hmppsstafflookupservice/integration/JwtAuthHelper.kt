b8dd35afe61f5a5d9e27296a242b141eeeede7d0
Andy Sweilam <104897795+AndySweilam@users.noreply.github.com>
2023-12-01 12:51:02 +0000

    refactor create Jwt (#82)
    * refactor create Jwt
    
    * fix klint issues
    
    * update oauth2ResourceServer from deprecated method.
    
    ---------
    
    Co-authored-by: AndySweilam <AndySweilam@users.noreply.github.com>

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsstafflookupservice/integration/JwtAuthHelper.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsstafflookupservice/integration/JwtAuthHelper.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsstafflookupservice.integration
 
 import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpHeaders
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder
@@ -55,11 +54,11 @@ class JwtAuthHelper {
       .also { scope?.let { scope -> it["scope"] = scope } }
       .let {
         Jwts.builder()
-          .setId(jwtId)
-          .setSubject(subject)
-          .addClaims(it.toMap())
-          .setExpiration(Date(System.currentTimeMillis() + expiryTime.toMillis()))
-          .signWith(keyPair.private, SignatureAlgorithm.RS256)
+          .id(jwtId)
+          .subject(subject)
+          .claims(it.toMap())
+          .expiration(Date(System.currentTimeMillis() + expiryTime.toMillis()))
+          .signWith(keyPair.private)
           .compact()
       }
 }
