566d2ce0d89b1a3b655bf1868f6b1643dfd35e60
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2024-10-04 16:28:56 +0100

    gradle 8.10.2 changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,8 +43,8 @@ java {
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-      jvmTarget = "19"
+    compilerOptions {
+      jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_19)
     }
   }
 }
