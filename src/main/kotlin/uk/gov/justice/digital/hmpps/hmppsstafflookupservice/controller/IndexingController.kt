f33ea59771e5681cf967db7c4d7e6a42d4f45ef4
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-04-03 13:17:42 +0100

    Hiding refresh endpoint from swagger docs

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstafflookupservice/controller/IndexingController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstafflookupservice/controller/IndexingController.kt
@@ -1,9 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsstafflookupservice.controller
 
+import io.swagger.v3.oas.annotations.Hidden
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -22,6 +24,7 @@ class IndexingController(
       ApiResponse(responseCode = "200", description = "OK"),
     ],
   )
+  @Hidden
   @PostMapping("/admin/refresh-staffs")
   suspend fun fullReindex(@RequestParam(defaultValue = "true") checkBuildRequired: Boolean) {
     indexingService.indexAll(checkBuildRequired)
