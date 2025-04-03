c0f0e3c32a36e5c7ca3c5a66705e08c485439fc5
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-04-03 13:28:28 +0100

    linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstafflookupservice/controller/IndexingController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsstafflookupservice/controller/IndexingController.kt
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.MediaType
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
